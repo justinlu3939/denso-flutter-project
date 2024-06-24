7cc85ae048df39b176d7c9f2bd2382f04acbfc0c
justinlu3939 <98128770+justinlu3939@users.noreply.github.com>
2024-06-24 13:59:29 -0500

    Added a carousel slider to the homepage

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/navbar.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 //might potentially use this dependency
 //import 'package:flutter_svg/flutter_svg.dart';
@@ -25,13 +26,32 @@ class HomePage extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return const SingleChildScrollView(
+    return SingleChildScrollView(
       child: SafeArea(
         child: Column(
           
           children: [
-            SizedBox(height: 20),
-            Text("Hello",),
+            const SizedBox(height: 20),
+            CarouselSlider(
+              items: [1,2,3,4].map((i) {
+              return Container(
+                //this makes each container widget width fit the entire screen
+                width: MediaQuery.of(context).size.width,
+                //this sets the space between each of the container widgets
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                decoration: BoxDecoration(
+                  //use the color gray to check the size of the widgets
+                  //color: Colors.grey[300],
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Center(
+                  child: Text(
+                    "text: $i"
+                  ),
+                ),
+              );
+            }).toList(), 
+            options: CarouselOptions(height: 250),)
           ],
         ),
       ),
