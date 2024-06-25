d4a417957a2a9fe483356b8e7012985337ac6b01
justinlu3939 <98128770+justinlu3939@users.noreply.github.com>
2024-06-25 15:49:02 -0500

    autoplay=true for carousel and created a indicator for carousel

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/navbar.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 //import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 //might potentially use this dependency
@@ -41,6 +42,7 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    int activeIndex = 0;
     return SingleChildScrollView(
       child: SafeArea(
         child: Column(
@@ -65,10 +67,19 @@ class HomePage extends StatelessWidget {
                   ),
                 );
               }).toList(),
-              options: CarouselOptions(height: 250),
+              options: CarouselOptions(height: 250,
+              autoPlay: true,
+              onPageChanged: (index, reason) {
+                activeIndex = index;
+              },
+              ),
             ),
             // const SizedBox(height: 10),
             // buildIndictor(),
+            AnimatedSmoothIndicator(activeIndex: activeIndex, 
+              count: 4, 
+              effect: const WormEffect(),
+            ),
           ],
         ),
       ),
