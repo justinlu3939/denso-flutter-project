423b608e030f6446aa086c810e3ddb081db361f3
justinlu3939 <98128770+justinlu3939@users.noreply.github.com>
2024-06-27 15:46:53 -0500

    improved the carousel code by adding images to it

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -32,7 +32,11 @@ class WelcomeText extends StatelessWidget
     return const Text(
             'Welcome to the Denso Homepage\nname of client',
             textAlign: TextAlign.left,
-            style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+            style: TextStyle(
+              fontSize: 20, 
+              //fontWeight: FontWeight.bold
+              //fontStyle: FontStyle.italic,
+            ),
           );
   }
 }
@@ -48,8 +52,13 @@ class HomePage extends StatelessWidget {
         child: Column(
           children: [
             const SizedBox(height: 20),
-            const WelcomeText(),
+            const Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: EdgeInsets.only(left: 20),
+                child: WelcomeText())),
             const SizedBox(height: 20),
+            //insert the carouself here
             CarouselSlider(
               items: [1, 2, 3, 4].map((i) {
                 return Container(
@@ -59,34 +68,68 @@ class HomePage extends StatelessWidget {
                   margin: const EdgeInsets.symmetric(horizontal: 20),
                   decoration: BoxDecoration(
                     //use the color gray to check the size of the widgets
-                    //color: Colors.grey[300],
+                    color: Colors.red,
                     borderRadius: BorderRadius.circular(20),
                   ),
                   child: Center(
-                    child: Text("text: $i"),
+                    //child: Text("text: $i"),
+                    child: ClipRect(
+                      //borderRadius: BorderRadius.circular(20),
+                      child: Image.asset('Images/example$i.jpg', fit: BoxFit.cover,)),
                   ),
                 );
               }).toList(),
-              options: CarouselOptions(height: 250,
-              autoPlay: true,
-              onPageChanged: (index, reason) {
-                activeIndex = index;
-              },
+                options: CarouselOptions(height: 250,
+                autoPlay: true,
+                onPageChanged: (index, reason) {
+                  activeIndex = index;
+                },
               ),
             ),
-            // const SizedBox(height: 10),
+            const SizedBox(height: 10),
             // buildIndictor(),
             AnimatedSmoothIndicator(activeIndex: activeIndex, 
               count: 4, 
               effect: const WormEffect(),
             ),
+            
           ],
         ),
       ),
     );
   }
-  // Widget buildIndictor() => AnimatedSmoothIndicator(
-  //   activeIndex: i, count: 4);
 }
 
-//https://www.denso.com/global/en/news/newsroom/2024/20240613-g01
+            // CarouselSlider(
+            //   items: [1, 2, 3, 4].map((i) {
+            //     return Container(
+            //       //this makes each container widget width fit the entire screen
+            //       width: MediaQuery.of(context).size.width,
+            //       //this sets the space between each of the container widgets
+            //       margin: const EdgeInsets.symmetric(horizontal: 20),
+            //       decoration: BoxDecoration(
+            //         //use the color gray to check the size of the widgets
+            //         color: Colors.grey[300],
+            //         borderRadius: BorderRadius.circular(20),
+            //       ),
+            //       child: Center(
+            //         //child: Text("text: $i"),
+            //         child: ClipRect(
+            //           //borderRadius: BorderRadius.circular(20),
+            //           child: Image.asset('Images/example$i.jpg', fit: BoxFit.cover,)),
+            //       ),
+            //     );
+            //   }).toList(),
+            //     options: CarouselOptions(height: 250,
+            //     autoPlay: true,
+            //     onPageChanged: (index, reason) {
+            //       activeIndex = index;
+            //     },
+            //   ),
+            // ),
+            // const SizedBox(height: 10),
+            // // buildIndictor(),
+            // AnimatedSmoothIndicator(activeIndex: activeIndex, 
+            //   count: 4, 
+            //   effect: const WormEffect(),
+            // ),
