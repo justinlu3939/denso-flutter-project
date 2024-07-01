2fcf7244b5d1a0f2e251fd474c4c69e627231da0
justinlu3939 <justinlu1@outlook.com>
2024-07-01 12:05:17 -0500

    Convert homepage to statefulwidget, changed list to data.dart items, added text to each of the carousel items, fixed the animated carousel indicator

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -24,73 +24,116 @@ class LandingPage extends StatelessWidget {
   }
 }
 
-class WelcomeText extends StatelessWidget
-{
+class WelcomeText extends StatelessWidget {
   const WelcomeText({super.key});
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
     return const Text(
-            'Welcome to the Denso Homepage\nname of client',
-            textAlign: TextAlign.left,
-            style: TextStyle(
-              fontSize: 20, 
-              //fontWeight: FontWeight.bold
-              //fontStyle: FontStyle.italic,
-            ),
-          );
+      'Welcome to the Denso Homepage\nname of client',
+      textAlign: TextAlign.left,
+      style: TextStyle(
+        fontSize: 20,
+        //fontWeight: FontWeight.bold
+        //fontStyle: FontStyle.italic,
+      ),
+    );
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int activeIndex = 0;
+  @override
   Widget build(BuildContext context) {
-    int activeIndex = 0;
     return SingleChildScrollView(
       child: SafeArea(
         child: Column(
           children: <Widget>[
             const SizedBox(height: 20),
             const Align(
-              alignment: Alignment.topLeft,
-              child: Padding(
-                padding: EdgeInsets.only(left: 20),
-                child: WelcomeText())),
+                alignment: Alignment.topLeft,
+                child: Padding(
+                    padding: EdgeInsets.only(left: 20), child: WelcomeText())),
             const SizedBox(height: 20),
             //insert the carouself here
             CarouselSlider(
-              items: [1, 2, 3, 4].map((i) {
+              items: dataList.map((item) {
                 return Container(
-                  //this makes each container widget width fit the entire screen
                   width: MediaQuery.of(context).size.width,
-                  //this sets the space between each of the container widgets
                   margin: const EdgeInsets.symmetric(horizontal: 20),
                   decoration: BoxDecoration(
-                    //use the color gray to check the size of the widgets
-                    color: Colors.red,
+                    //color: Theme.of(context).primaryColor,
+                    color: Colors.red[400],
                     borderRadius: BorderRadius.circular(20),
                   ),
-                  child: Center(
-                    //child: Text("text: $i"),
-                    child: ClipRect(
-                      //borderRadius: BorderRadius.circular(20),
-                      child: Image.asset('Images/example$i.jpg', fit: BoxFit.cover,)),
+                  child: Column(
+                    children: <Widget>[
+                      Stack(
+                        children: <Widget>[
+                          Center(
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(20),
+                              child: Image.asset(
+                                item.imageName,
+                                fit: BoxFit.cover,
+                                height: 190,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                item.title,
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                item.description,
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 );
               }).toList(),
-                options: CarouselOptions(height: 250,
+              options: CarouselOptions(
+                height: 300,
                 autoPlay: true,
                 onPageChanged: (index, reason) {
-                  activeIndex = index;
+                  setState(() {
+                    activeIndex = index;
+                  });
                 },
               ),
             ),
+
             const SizedBox(height: 20),
             // buildIndictor(),
-            AnimatedSmoothIndicator(activeIndex: activeIndex, 
-              count: 4, 
+            AnimatedSmoothIndicator(
+              activeIndex: activeIndex,
+              count: 4,
               effect: const WormEffect(
                 dotColor: Colors.red,
                 activeDotColor: Colors.green,
