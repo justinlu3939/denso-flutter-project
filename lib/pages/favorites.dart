d70e7b84146b359d73a1a8f5a5806fa6cd5de910
justinlu3939 <98128770+justinlu3939@users.noreply.github.com>
2024-07-31 14:54:17 -0500

    Completed the favorites tab. It only produced list tiles with isFavorite as true

--- lib/pages/favorites.dart
+++ lib/pages/favorites.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:my_app/components/filterItemData.dart';
 
 class Favorites extends StatefulWidget {
   const Favorites({super.key});
@@ -10,8 +12,8 @@ class Favorites extends StatefulWidget {
 class _FavoritesState extends State<Favorites> {
   @override
   Widget build(BuildContext context) {
+    List<ProductData> products = getProductData();
     return Scaffold(
-      
       appBar: AppBar(
         leading: IconButton(
           icon: const Icon(Icons.arrow_back_ios),
@@ -21,43 +23,61 @@ class _FavoritesState extends State<Favorites> {
           'Favorites',
           //this changes the font, etc. of a text directly
           style: TextStyle(
-            fontSize: 40,
+            fontSize: 25,
             fontWeight: FontWeight.bold,
           ),
         ),
         shadowColor: Colors.black12,
         centerTitle: true,
       ),
-      body: const Placeholder(),
-    );
-  }
-}
-
-class ScrollView extends StatelessWidget {
-  const ScrollView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          ListTile(
-            contentPadding: const EdgeInsets.all(20),
-            leading: Image.asset('Images/airfilter.png', width: 90, height: 90,),
-            title: const Text(
-              'Air Filter',
-              style: TextStyle(
-                fontSize: 20,
-                fontFamily: 'Times New Roman',
-              ),
-            ),
-            subtitle: const Text('These filters are used to to clean the air that circulates through your heating and cooling system.'),
-            //tileColor: Colors.blue,
-            hoverColor: Colors.red,
-            focusColor: Colors.white,
-            trailing: const Icon(Icons.arrow_forward_ios),
-          ),
-        ],
+      body: ValueListenableBuilder(
+        valueListenable: Hive.box('favorites').listenable(), 
+        builder: (context, box, child){
+          List<ProductData> favoriteProducts = products.where((product) {
+            final index = products.indexOf(product);
+            return box.get(index) != null;
+          }).toList();
+          return ListView.builder(
+            itemCount: favoriteProducts.length,
+            itemBuilder: (context, index) {
+              final product = favoriteProducts[index];
+              final isFavorite = box.get(products.indexOf(product)) != null;
+                return ListTile(
+                contentPadding: const EdgeInsets.all(20),
+                leading: Image.asset(product.image, width: 90, height: 90,),
+                title: Text(
+                  product.name,
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontFamily: 'Times New Roman',
+                  ),
+                ),
+                subtitle: Text(product.description),
+                //tileColor: Colors.blue,
+                hoverColor: Colors.red,
+                focusColor: Colors.white,
+                trailing: IconButton(onPressed: () async {
+                  ScaffoldMessenger.of(context).clearSnackBars();
+                  if(isFavorite) {
+                    await box.delete(index);
+                    const snacky = SnackBar(content: Text('Removed from Favorites'), backgroundColor: Colors.red,);
+                    ScaffoldMessenger.of(context).showSnackBar(snacky);
+                  }
+                  else {
+                    await box.put(index, product.name);
+                    const snacky = SnackBar(content: Text('Added to Favorites'), backgroundColor: Colors.blue,);
+                    ScaffoldMessenger.of(context).showSnackBar(snacky);
+                  }
+                } ,
+                icon: Icon(isFavorite ? Icons.favorite : Icons.favorite_border_outlined), color: Colors.red,),
+                onTap: () {
+                  Navigator.pushNamed(context, '/webviewcontainer',
+                                      arguments: product.arguments);
+                },
+              );
+            }
+          );
+        }
       ),
     );
   }
